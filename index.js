0d0e1fe0f4deee65716692e9333a403c12c5b50c
ye-geeee <yeaji9683@gmail.com>
2020-04-10 06:23:38 +0900

    Add SMTP Module

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+});
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-  }).listen(3000);
+app.listen(3000);
