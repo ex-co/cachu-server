65f1ebec693be25b8ae4c9eec806cfac55f81785
qwebnm7788 <qwebnm7788@naver.com>
2020-04-18 11:19:06 +0900

    [Logging] Add Custom logging module
    - Add custom logging module using winston library
    - Use [timestamp] [file] [label] : [message] format
    
    Signed-off-by: qwebnm7788 <qwebnm7788@naver.com>

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const L = require('./lib/logging/CustomLogger')('index.js');
 
 app.get('/', (request, response) => {
+    L.log('info', 'test info');
     response.send("<h1>Hello Cachu</h1>");
 });
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
+    L.log('info', "Server is running");
     console.log(`Server is running on port ${PORT}`);
 });
